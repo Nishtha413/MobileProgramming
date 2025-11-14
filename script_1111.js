7653ef028d6a300cb1d09d93962fb498ac0269ec
Nishtha <nishthamhjrn@gmail.com>
2025-11-14 22:09:53 +0545

    added November 11 classwork

--- script_1111.js
+++ script_1111.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#show").click(function(){
-    $("div").show();
+    $("#yes").show();
     }); 
 
     $("p").hover(function(){
@@ -12,7 +12,23 @@ $(document).ready(function(){
         $(this).css("font-size", "30px")
     });
     $("#a").click(function(){
-        $("*").hide();
+        $("#no").hide();
     });
-    
-});
+
+    //november 13
+    $(".button").click(function(){
+        $(".a").animate({left: '250px'},400,function(){
+            $(this).css("background-color", "red")
+        }).animate({height:'250px'},400,function(){
+            $(".a").css("background-color","blue")
+        }).animate({width:'350px'},400,function(){
+            $(".a").css("background-color","green")
+        }).animate({fontSize:"40px"},400,function(){
+            $(".a").css("background-color","pink");
+        }).animate({height:'100px',opacity:'0.5'},400,function(){
+            $(this).css("background-color","aqua");
+        }).animate({height:'200px',width:'200px',fontSize:'16px', opacity:'1', left:0},400,function(){
+            $(this).css("background-color","#89CC04");
+        })
+    });
+    });
